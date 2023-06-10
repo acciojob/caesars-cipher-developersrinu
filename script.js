116720ebde77d9539c67270445aad954ce2afe3c
developersrinu <132943635+developersrinu@users.noreply.github.com>
2023-06-10 22:38:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,15 @@
-// Your Script here.
 
-const lookup = {
-  'A': 'N','B': 'O','C': 'P','D': 'Q',
-  'E': 'R','F': 'S','G': 'T','H': 'U',
-  'I': 'V','J': 'W','K': 'X','L': 'Y',
-  'M': 'Z','N': 'A','O': 'B','P': 'C',
-  'Q': 'D','R': 'E','S': 'F','T': 'G',
-  'U': 'H','V': 'I','W': 'J','X': 'K',
-  'Y': 'L','Z': 'M', '?': '?', ',': ','
-};
+function rot13(encodedStr) {
+  let decodedArr = [];
 
-function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
+  for (let char of encodedStr) {
+    let decodedChar = lookup[char] || char;
+    decodedArr.push(decodedChar);
+  }
 
-  return ;//return decodedArr
+  return decodedArr.join('');
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
+console.log(rot13("SERR YBIR? NPPVBWBO"));  // Output: "FREE LOVE? ACCOUNTING"
 
 module.exports = rot13;
